7cd6a1080b8cae36e6fd1ab53812737f14f40383
IN-GU1LPW10055\gaurav <gaurav.gera@taistech.com>
2018-03-26 08:06:32 +0530

    first Component, root component created

---
+++ js/clientApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+function App() {
+    return (
+        <div>
+            <h1>Hi???</h1>
+        </div>
+    );
+}
+render(<App/>, document.getElementById('app'));
